d1af779f392d13f878257e51088236941f02cf7c
J1ngli <163376858+J1ngli@users.noreply.github.com>
2024-10-08 20:05:53 +0000

    Milestone1

--- main.c
+++ main.c
@@ -2,12 +2,40 @@
 #include <stdint.h>
 
 int32_t is_ascii(char str[]){
-
+    int i =0;
+    while(str[i]!='\0'){
+        if((unsigned char)str[i]>127){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
 }
 
 int32_t capitalize_ascii(char str[]){
+    int i=0;
+    int n_changes=0;
+    while(str[i]!='\0'){
+        if(str[i]<='z'&&str[i]>='a'){
+            str[i]-=32;
+            n_changes++;
+        }
+        i++;
+    }
+    return n_changes;
+}
+
+int32_t width_from_start_byte(char start_byte){
     
 }
 int main(){
+    printf("Is 🔥 ASCII? %d\n", is_ascii("🔥"));
+    printf("Is abcd ASCII? %d\n", is_ascii("abcd"));
+    
+    int32_t ret = 0;
+    char str[] = "abcd";
+    ret = capitalize_ascii(str);
+    printf("Capitalized String: %s\nCharacters updated: %d\n", str, ret);
 
+    return 0;
 }
